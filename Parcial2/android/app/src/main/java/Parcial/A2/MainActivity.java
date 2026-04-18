50b609a62220714b53508395b67dab91e4aaa869
David Astudillo <david.astudillo@uao.edu.co>
2026-04-18 17:17:10 -0500

    Eliminar tests con error y sincronizar appId de Android

--- Parcial2/android/app/src/main/java/Parcial/A2/MainActivity.java
+++ Parcial2/android/app/src/main/java/Parcial/A2/MainActivity.java
@@ -1,4 +1,4 @@
-package io.ionic.starter;
+package Parcial.A2;
 
 import com.getcapacitor.BridgeActivity;
 
--- Parcial2/capacitor.config.ts
+++ Parcial2/capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'Parcial.A2',
   appName: 'Parcial2',
   webDir: 'dist'
 };
